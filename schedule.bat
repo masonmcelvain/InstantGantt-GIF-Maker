1bd4ec665b0e9e9a776df26dc84c94c6720c2887
lekjos <leif@leifkjos.com>
2022-03-19 13:50:21 -0700

    update bat file to use %user_profile%

--- schedule.bat
+++ schedule.bat
@@ -1,2 +1,2 @@
-cd "C:\Users\leifk\Documents\GitHub\LocalLogic\"
-"C:\Users\leifk\Documents\GitHub\LocalLogic\env\Scripts\python.exe" "C:\Users\leifk\Documents\GitHub\LocalLogic\main.py"
+cd "%USER_PROFILE%\Documents\GitHub\LocalLogic\"
+"%USER_PROFILE%\Documents\GitHub\LocalLogic\env\Scripts\python.exe" "%USER_PROFILE%\Documents\GitHub\LocalLogic\main.py"

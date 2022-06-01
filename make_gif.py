269fded2a808612675d22084bac1e4c163607d68
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2022-06-01 08:44:16 -0700

    make_gif: Read from img dir
    And add some output info

--- make_gif.py
+++ make_gif.py
@@ -1,7 +1,8 @@
-import imageio
 import configparser, os, datetime
-import glob
+from pathlib import Path
 from PIL import Image
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 #get filenames
 config= configparser.ConfigParser()
@@ -10,22 +11,31 @@ prog_config.read('program_config.ini')
 config.read('project_config.ini')
 file_list = []
 save_dir = os.path.join(os.environ['USERPROFILE'], 'Pictures', 'Local_Logic_Export')
+#make save directory if needed
+Path(save_dir).mkdir(parents=True, exist_ok=True)
+print(f"Saving to {save_dir}")
+
+if prog_config['Paths']['save_directory'][0] == '~':
+    img_dir = os.path.expanduser(prog_config['Paths']['save_directory'])
+else:
+    img_dir = prog_config['Paths']['save_directory']
+print(f"Reading images in {img_dir} from {ROOT_DIR}")
+
 for project in config:
     if project == 'DEFAULT':
             continue
     
     project_files = []
     workspace_name = config[project]['workspace']
-    for filename in os.listdir(save_dir):
+    for filename in os.listdir(img_dir):
         if filename.startswith("_".join([workspace_name, project])):
-            project_files.append(os.path.join(save_dir,filename))
-
+            project_files.append(os.path.join(img_dir, filename))
 
     save_as_name = "_".join([workspace_name, project, datetime.date.today().strftime('%Y-%m-%d')]) + ".gif"
     fp_out = os.path.join(save_dir,save_as_name)
 
-
-
     img, *imgs = [Image.open(f) for f in project_files]
     img.save(fp=fp_out, format='GIF', append_images=imgs,
          save_all=True, duration=200, loop=0)
+
+    print(f"Saved {project} successfully")
